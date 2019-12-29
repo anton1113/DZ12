27f0d62b13d1041b83fb1e81ecbf301b1ca1bd70
SM <smserg2000@gmail.com>
2019-12-29 19:02:49 +0200

    commit-2 Подсчитывается общее количество слов в файле.

--- src/MyFile.java
+++ src/MyFile.java
@@ -1,2 +1,14 @@
+import java.io.*;
+
 public class MyFile {
+    public static void main(String[] args) throws  IOException{
+        int wordcount=0;
+        FileReader file = new FileReader("C://COURSE//dz12//src//javaprogram.txt");
+        StreamTokenizer fileTokenizer =new StreamTokenizer(file);
+        while ((fileTokenizer.nextToken()) !=StreamTokenizer.TT_EOF){
+            if(fileTokenizer.ttype==StreamTokenizer.TT_WORD)
+                wordcount++;
+        }
+        System.out.println("Количество слов в файле: " +wordcount);
+    }
 }
